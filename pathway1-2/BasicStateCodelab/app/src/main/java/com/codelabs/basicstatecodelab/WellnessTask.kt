3c20640682eebc6340251c96752b1ab781fe147c
INAH-PAK <inahpakkr@gmail.com>
2022-11-27 20:08:40 +0900

    ViewModel error test

--- pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
+++ pathway1-2/BasicStateCodelab/app/src/main/java/com/codelabs/basicstatecodelab/WellnessTask.kt
@@ -4,10 +4,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
-data class WellnessTask(
+class WellnessTask(
     val id: Int,
     val label: String,
     initialChecked: Boolean = false
 ) {
-    var checked by mutableStateOf(initialChecked)
+    var checked: Boolean by mutableStateOf(initialChecked)
 }
